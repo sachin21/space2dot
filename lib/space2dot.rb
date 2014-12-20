0bc778cd93ed2596883f9b56ccd4efea69da8abc
Satoshi Ohmori <sachin21dev@gmail.com>
2014-12-21 02:34:18 +0900

    Become self all

--- lib/space2dot.rb
+++ lib/space2dot.rb
@@ -1,11 +1,13 @@
 require 'space2dot/version'
 
 module Space2dot
-  def self.convert(argv)
-    argv.length == 1 ? argv[0].strip.gsub(/\s/,  '.').insert(0,  '.') : argv.join('.').insert(0, '.')
-  end
+  class << self
+    def convert(argv)
+      argv.length == 1 ? argv[0].strip.gsub(/\s/,  '.').insert(0,  '.') : argv.join('.').insert(0, '.')
+    end
 
-  def self.copy_command(dot_include_sentence)
-    "echo #{dot_include_sentence} | ruby -pe 'chomp' | pbcopy"
+    def copy_command(dot_include_sentence)
+      "echo #{dot_include_sentence} | ruby -pe 'chomp' | pbcopy"
+    end
   end
 end
