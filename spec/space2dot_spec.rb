20cfce792cd1f50e15fc21724a7096095bb6ea07
Satoshi Ohmori <sachin21dev@gmail.com>
2015-08-31 00:01:06 +0900

    Refactor for specification

--- spec/space2dot_spec.rb
+++ spec/space2dot_spec.rb
@@ -1,12 +1,8 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'spec_helper'
 
 describe Space2dot do
-  it 'Successful execution' do
+  it 'should be Successful execution' do
     expect(Space2dot.convert(['fuga hoge foo'])).to include('.') # String case
     expect(Space2dot.convert(%w(fuga hoge foo))).to include('.') # Array case
-  end
-
-  it 'Successful copied' do
-    expect(Space2dot.generate_command('.fuga.hoge.foo')).to eq("echo .fuga.hoge.foo | ruby -pe 'chomp' | pbcopy")
   end
 end
