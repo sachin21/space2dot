c6285bb459e150d16ff7fe91dec49e6e8de7c3eb
Satoshi Ohmori <sachin21dev@gmail.com>
2015-09-09 19:03:13 +0900

    Fix title for spec

--- spec/space2dot_spec.rb
+++ spec/space2dot_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Space2dot do
-  it 'should be Successful execution' do
+  it 'returns dot included in string' do
     expect(Space2dot.convert(['fuga hoge foo'])).to include('.') # String case
     expect(Space2dot.convert(%w(fuga hoge foo))).to include('.') # Array case
   end
