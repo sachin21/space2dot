5afb7c0e4aadf399b80508924953470ced32c7b1
Satoshi Ohmori <sachin21dev@gmail.com>
2015-10-05 13:25:23 +0900

    Modify config for use in tmuxinator

--- spec/space2dot_spec.rb
+++ spec/space2dot_spec.rb
@@ -5,4 +5,9 @@ describe Space2dot do
     expect(Space2dot.convert(['fuga hoge foo'])).to include('.') # String case
     expect(Space2dot.convert(%w(fuga hoge foo))).to include('.') # Array case
   end
+
+  it 'returns dot included in string in beginning of line' do
+    expect(Space2dot.convert(['fuga hoge foo'])[0]).to eq('.')
+    expect(Space2dot.convert(%w(fuga hoge foo))[0]).to eq('.')
+  end
 end
