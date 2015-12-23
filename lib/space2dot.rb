e4b9e4c1223348b3002065bd48464f4b413ac378
Satoshi Ohmori <sachin21dev@gmail.com>
2015-12-23 11:21:26 +0900

    Fix coding styles according to Rubocop

--- lib/space2dot.rb
+++ lib/space2dot.rb
@@ -1,7 +1,14 @@
+# coding: utf-8
+
 require 'space2dot/version'
 
+# This is the main functions.
 module Space2dot
   def self.convert(argv)
-    argv.length == 1 ? argv[0].strip.gsub(/\s/, '.').insert(0, '.') : argv.join('.').insert(0, '.')
+    if argv.length == 1
+      argv[0].strip.gsub(/\s/, '.').insert(0, '.')
+    else
+      argv.join('.')
+    end.insert(0, '.')
   end
 end
